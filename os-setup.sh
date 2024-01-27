e821fbca221e37c03d0a8fc7d04c4578285fbb14
viktorybloom <viktorsulejic@gmail.com>
2024-01-27 10:07:22 +1000

    cleanup

--- os-setup.sh
+++ os-setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Variables
-sd_card_device=$(ls /dev/sd* | grep -E "/dev/sd[b-z]" | head -n 1) # from b-z since sata takes sd'a'
+sd_card_device=$(ls /dev/sd* | grep -E "/dev/sd[b-z]" | head -n 1) # from b-z since sdcard reader itself takes sd'a'
 vault_file="./vault/secret.txt"
 target_password=$(echo "$(cat $vault_file)" | mkpasswd -m sha-512 -s)
 target_name=$(grep "target_name:" "./vars.yaml" | awk -F"'" '{print $2}')
@@ -16,6 +16,18 @@ sudo mount ${sd_card_device}"1" /mnt   # Assuming boot partition is the first pa
 # Enable SSH on Raspberry Pi
 sudo touch /mnt/ssh
 
+# Edit netplan to enable eth0 wired connection on RPI with ubuntu server iso. 
+echo -e "
+    version: 2
+    ethernets:
+      eth0:
+        dhcp4: true
+        dhcp4-overrides:
+          use-dns: false
+        nameservers:
+          addresses: [127.0.0.1, 9.9.9.9] # Required for docker pihole dns service
+        optional: true" | sudo tee /mnt/netplan
+	
 # Edit user-data to setup initial credentials
 echo -e "#cloud-config\n\n\
 hostname: ${target_name}n\
